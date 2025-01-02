fe2f02a9c074c4fc2154d90f4c23b2ef9c5cb9b2
MostRealJarinMusic <joshjarin2006@gmail.com>
2025-01-02 23:05:16 +0000

    Operators and better typechecking

--- Main.hs
+++ Main.hs
@@ -10,9 +10,13 @@ main = do
     return ()
 
 testProgram :: Expr
-testProgram = Seq (Set "x" (Lit (VInt 7))) (Seq (Set "y" (Lit (VString "6"))) (Set "z" (BinOp Add (Get "x") (Get "y"))))
+--testProgram = Seq (Set "x" (Lit (VInt 7))) (Seq (Set "y" (Lit (VString "6"))) (Set "z" (BinOp Add (Get "x") (Get "y"))))
 --testProgram = Set "x" (BinOp Add (Lit (VInt 7)) (Lit (VString "6")))
 --testProgram = Seq (Set "x" (Lit (VInt 42))) (Print (Get "x"))
+--testProgram = Seq (Set "msg" (Lit (VString "Hello"))) (Seq (Set "count" (Lit (VInt 3))) (While (BinOp Gt (Get "count") (Lit (VInt 0))) (Seq (Print (Get "msg")) (Set "count" (BinOp Sub (Get "count") (Lit (VInt 1)))))))
+--testProgram = Seq (Set "x" (Lit (VInt 10))) (While (BinOp Gt (Get "x") (Lit (VInt 0))) (Seq (Set "x" (BinOp Sub (Get "x") (Lit (VInt 1)))) (Print (Get "x"))))
+--testProgram = Print (Get "undefinedVar")
+testProgram = Seq (Set "msg" (Lit (VString "Hello"))) (Seq (Set "count" (Lit (VInt 10))) (DoWhile (BinOp Gt (Get "count") (Lit (VInt 0))) (Seq (Print (Get "msg")) (Set "count" (BinOp Sub (Get "count") (Lit (VInt 1)))) )))
 
 evaluateProgram :: Expr -> IO (Value, Variables)
 evaluateProgram = evaluate empty 
@@ -161,6 +165,7 @@ evalUnOps Not _            = Left "Type error: Incorrect type for logical NOT"
 
 evalBinOps :: BinOp -> Value -> Value -> Either String Value
 evalBinOps Add (VInt x) (VInt y) = Right $ VInt (x + y) 
+evalBinOps Add (VString x) (VString y) = Right $ VString (x ++ y) 
 evalBinOps Add _        _        = Left "Type error: incorrect types for Add"
 evalBinOps Mul (VInt x) (VInt y) = Right $ VInt (x * y)
 evalBinOps Sub (VInt x) (VInt y) = Right $ VInt (x - y)
@@ -170,6 +175,22 @@ evalBinOps Div (VInt x) (VInt y)
 evalBinOps Mod (VInt x) (VInt y) 
     | y /= 0                     = Right $ VInt (x `mod` y)
     | otherwise                  = Left "Runtime error: attempt to perform modulo division by 0"
+evalBinOps Eq (VInt x) (VInt y)  = Right $ VBool $ x == y
+evalBinOps Neq (VInt x) (VInt y)  = Right $ VBool $ x /= y
+evalBinOps Lt (VInt x) (VInt y)  = Right $ VBool $ x < y
+evalBinOps Lte (VInt x) (VInt y)  = Right $ VBool $ x <= y
+evalBinOps Gt (VInt x) (VInt y)  = Right $ VBool $ x > y
+evalBinOps Gte (VInt x) (VInt y)  = Right $ VBool $ x >= y
+evalBinOps Eq (VBool x) (VBool y)  = Right $ VBool $ x == y
+evalBinOps Neq (VBool x) (VBool y)  = Right $ VBool $ x /= y
+evalBinOps Lt (VBool x) (VBool y)  = Right $ VBool $ x < y
+evalBinOps Lte (VBool x) (VBool y)  = Right $ VBool $ x <= y
+evalBinOps Gt (VBool x) (VBool y)  = Right $ VBool $ x > y
+evalBinOps Gte (VBool x) (VBool y)  = Right $ VBool $ x >= y
+evalBinOps Eq (VString x) (VString y)  = Right $ VBool $ x == y
+evalBinOps Neq (VString x) (VString y)  = Right $ VBool $ x /= y
+evalBinOps And (VBool x) (VBool y)  = Right $ VBool $ x && y
+evalBinOps Or (VBool x) (VBool y)  = Right $ VBool $ x || y
 evalBinOps op  _        _        = Left $ "Runtime error: attempt to apply " ++ show op ++ " with incorrect types"
 
 
@@ -180,11 +201,13 @@ typecheck _ (Lit (VBool   _)) = Right TBool
 typecheck _ (Lit (VString _)) = Right TString
 
 typecheck vars (Set name expr) = do
-    exprType <- typecheck vars expr
-    case lookup name vars of 
-        Just (existingType, _) | existingType /= exprType -> 
-            Left ("Type error in Set: Variable " ++ name ++ " has type " ++ show existingType ++ " but the expression evaluates to " ++ show exprType)
-        _ -> Right exprType
+    let exprType = typecheck vars expr
+    case exprType of
+        Left err -> Left err
+        Right exprType' -> case lookup name vars of 
+            Just (existingType, _) | existingType /= exprType' -> 
+                Left ("Type error in Set: Variable " ++ name ++ " has type " ++ show existingType ++ " but the expression evaluates to " ++ show exprType)
+            _ -> Right TNone
 
 typecheck vars (Get name) = do
     case lookup name vars of
@@ -192,46 +215,96 @@ typecheck vars (Get name) = do
         Just (existingType, value) -> Right existingType
 
 typecheck vars (BinOp op expr1 expr2) = do
-    exprType1 <- typecheck vars expr1
-    exprType2 <- typecheck vars expr2
+    let exprType1 = typecheck vars expr1
+    let exprType2 = typecheck vars expr2
     case (op, exprType1, exprType2) of
-        (Add, TInt, TInt) -> Right TInt
-        (Mul, TInt, TInt) -> Right TInt
-        (Sub, TInt, TInt) -> Right TInt
-        (Div, TInt, TInt) -> Right TInt
-        (Mod, TInt, TInt) -> Right TInt
+        (Add, Right TInt, Right TInt) -> Right TInt
+        (Add, Right TString, Right TString) -> Right TString
+        (Mul, Right TInt, Right TInt) -> Right TInt
+        (Sub, Right TInt, Right TInt) -> Right TInt
+        (Div, Right TInt, Right TInt) -> Right TInt
+        (Mod, Right TInt, Right TInt) -> Right TInt
+        (Eq, Right TInt, Right TInt)  -> Right TBool
+        (Neq, Right TInt, Right TInt)  -> Right TBool
+        (Lt, Right TInt, Right TInt)  -> Right TBool
+        (Lte, Right TInt, Right TInt)  -> Right TBool
+        (Gt, Right TInt, Right TInt)  -> Right TBool
+        (Gte, Right TInt, Right TInt)  -> Right TBool
+        (Eq, Right TBool, Right TBool)  -> Right TBool
+        (Neq, Right TBool, Right TBool)  -> Right TBool
+        (Lt, Right TBool, Right TBool)  -> Right TBool
+        (Lte, Right TBool, Right TBool)  -> Right TBool
+        (Gt, Right TBool, Right TBool)  -> Right TBool
+        (Gte, Right TBool, Right TBool)  -> Right TBool
+        (Eq, Right TString, Right TString)  -> Right TBool
+        (Neq, Right TString, Right TString)  -> Right TBool
+        (And, Right TBool, Right TBool)  -> Right TBool
+        (Or, Right TBool, Right TBool)  -> Right TBool
+        (_,  Left err1, Left err2) -> Left $ err1 ++ " and " ++ err2
+        (_, Left err, _) -> Left err
+        (_, _, Left err) -> Left err
         _                 -> Left (show op ++ " cannot be applied to types " ++ show exprType1 ++ " and " ++ show exprType2)
     
 typecheck vars (UnOp op expr) = do
-    exprType <- typecheck vars expr
+    let exprType = typecheck vars expr
     case (op, exprType) of
-        (Neg, TInt) -> Right TInt
+        (Neg, Right TInt) -> Right TInt
+        (Not, Right TBool) -> Right TBool
         _                 -> Left ("Incorrect type for function " ++ show op)
 
 typecheck vars (IfElse pred expr1 expr2) = do
-    predicateType <- typecheck vars pred
-    unless (predicateType == TBool) (Left "Incorrect type for if-else condition")
-    exprType1 <- typecheck vars expr1
-    exprType2 <- typecheck vars expr2
+    {-
+    let predType = typecheck vars pred
+    unless (predType == Right TBool) (Left "Incorrect type for if-else condition")
+    let exprType1 = typecheck vars expr1
+    let exprType2 = typecheck vars expr2
     if exprType1 == exprType2 
-    then return exprType1
+    then exprType1
     else Left "Block types for if-else statement do not match"
+    -}
+    checkThrough vars TNone [(pred, TBool, "if-else statement"), (expr1, TBool, "if block"), (expr2, TBool, "else block")]
+
+typecheck vars (Seq expr1 expr2) = do
+    --let exprType1 = typecheck vars expr1
+    --unless (exprType1 == Right TNone) $ Left "Incorrect type for sequence block"
+    checkThrough vars TNone [(expr1, TNone, "sequence block")]
+    typecheck vars expr2
+    
 
 typecheck vars (While pred expr) = do
-    predType <- typecheck vars pred
-    unless (predType == TBool) (Left "Incorrect type for while condition")
-    exprType <- typecheck vars expr
-    unless (exprType == TNone) (Left "Incorrect type within while loop")
-    Right TNone
+    {-
+    case typecheck vars pred of
+        Left str -> Left str
+        Right TBool -> case typecheck vars expr of
+            Left str -> Left str
+            Right TNone -> Right TNone
+            Right x -> Left $ "Type error: incorrect type " ++ show x ++ " within while loop"
+        Right x -> Left $ "Type error: incorrect type " ++ show x ++ " within while condition"
+    -}
+    checkThrough vars TNone [(pred, TBool, "while condition"), (expr, TNone, "while loop")]
+
 
 typecheck vars (DoWhile pred expr) = do
-    predType <- typecheck vars pred
-    unless (predType == TBool) (Left "Incorrect type for do-while condition")
-    exprType <- typecheck vars expr
-    unless (exprType == TNone) (Left "Incorrect type within do-while loop")
+    {-
+    let predType = typecheck vars pred
+    unless (predType == Right TBool) (Left "Incorrect type for do-while condition")
+    let exprType = typecheck vars expr
+    unless (exprType == Right TNone) (Left "Incorrect type within do-while loop")
     Right TNone
+    -}
+    checkThrough vars TNone [(pred, TBool, "do-while condition"), (expr, TNone, "do-while loop")]
 
 typecheck vars (Print expr) = do
     case typecheck vars expr of 
         Left error -> Left error
         Right _  -> Right TNone
+
+
+--Helper functions for typechecking
+checkThrough :: Variables -> Type -> [(Expr, Type, String)] -> Either String Type
+checkThrough _    x []                                 = Right x
+checkThrough vars x ((expr, expectedType, failStr):es) = do
+    case typecheck vars expr of
+        Left str -> Left str
+        Right testType | testType == expectedType -> do checkThrough vars x es
+        Right x -> Left $ "Type error: incorrect type " ++ show x ++ " within " ++ failStr
