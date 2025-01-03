8b0d2912df85cf6c8ab47c13ff0ce4d5236de7bc
MostRealJarinMusic <joshjarin2006@gmail.com>
2025-01-03 12:42:21 +0000

    Housekeeping

--- Definitions.hs
+++ Definitions.hs
@@ -3,63 +3,67 @@ import Data.Map (Map)
 
 --Here, we define the AST
 --Loosely based on Haskell's Core
-data Expr = Set String Expr 
-            | Get String
-            | Lit Value
-            | BinOp BinOp Expr Expr
-            | UnOp UnOp Expr
-            | IfElse Expr Expr Expr
-            | Seq [Expr]
-            | While Expr Expr
-            | DoWhile Expr Expr
-            | Skip
-            | Print Expr
-    deriving (Show)
+data Expr 
+  = Set String Expr 
+  | Get String
+  | Lit Value
+  | BinOp BinOp Expr Expr
+  | UnOp UnOp Expr
+  | IfElse Expr Expr Expr
+  | Seq [Expr]
+  | While Expr Expr
+  | DoWhile Expr Expr
+  | Skip
+  | Print Expr
+  deriving (Show)
+
+
 
 --Defining binary operations
-data BinOp = Add | Mul | Sub | Div | Mod 
-            | Eq | Neq | Lt | Lte | Gt | Gte 
-            | And | Or  
-    deriving (Show)
+data BinOp 
+  = Add | Mul | Sub | Div | Mod 
+  | Eq | Neq | Lt | Lte | Gt | Gte 
+  | And | Or  
+  deriving (Show)
 
 --Defining unary operations
-data UnOp = 
-    Neg
-    | Not
-    deriving (Show)
+data UnOp 
+  = Neg
+  | Not
+  deriving (Show)
 
 --Defining values with wrappers
-data Value = 
-    VInt Int
-    | VString String
-    | VBool Bool
-    | VNone
-    | VError
-    deriving (Eq, Show)
+data Value 
+  = VInt Int
+  | VString String
+  | VBool Bool
+  | VNone
+  | VError
+  deriving (Eq, Show)
 
 --Defining data types
-data Type = 
-    TInt
-    | TString
-    | TBool 
-    | TNone
-    deriving (Eq, Show)
+data Type 
+  = TInt
+  | TString
+  | TBool 
+  | TNone
+  deriving (Eq, Show)
 
 --Defining variables as a dictionary of 'names' to 'type-values'
 type Variables = Map String (Type, Value)
 
 --Defining tokens
-data Token = 
-    TokInt Int
-    | TokString String
-    | TokBool Bool
-    | TokEOF
-    | TokKeyword String
-    | TokIdentifier String
-    | TokSymbol String
-    | TokLeftBracket
-    | TokRightBracket
-    | TokLeftBrace
-    | TokRightBrace
-    | TokSemicolon
-    deriving (Show, Eq)
+data Token 
+  = TokInt Int
+  | TokString String
+  | TokBool Bool
+  | TokEOF
+  | TokKeyword String
+  | TokIdentifier String
+  | TokSymbol String
+  | TokLeftBracket
+  | TokRightBracket
+  | TokLeftBrace
+  | TokRightBrace
+  | TokSemicolon
+  deriving (Show, Eq)
--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ import Prelude hiding (lookup)
 import Control.Monad ( unless )
 import Data.Map ( Map, insert, lookup, empty )
 import Definitions
+import Typechecker
 
 main :: IO ()
 main = do
@@ -226,93 +227,3 @@ evalBinOps Neq (VString x) (VString y)  = Right $ VBool $ x /= y
 evalBinOps And (VBool x) (VBool y)  = Right $ VBool $ x && y
 evalBinOps Or (VBool x) (VBool y)  = Right $ VBool $ x || y
 evalBinOps op  _        _        = Left $ "Runtime error: attempt to apply " ++ show op ++ " with incorrect types"
-
-
---Typechecking
-typecheck :: Variables -> Expr -> Either String Type
-typecheck _ (Lit (VInt    _)) = Right TInt
-typecheck _ (Lit (VBool   _)) = Right TBool
-typecheck _ (Lit (VString _)) = Right TString
-
-typecheck vars (Set name expr) = do
-    let exprType = typecheck vars expr
-    case exprType of
-        Left err -> Left err
-        Right exprType' -> case lookup name vars of 
-            Just (existingType, _) | existingType /= exprType' -> 
-                Left ("Type error in Set: Variable " ++ name ++ " has type " ++ show existingType ++ " but the expression evaluates to " ++ show exprType)
-            _ -> Right TNone
-
-typecheck vars (Get name) = do
-    case lookup name vars of
-        Nothing -> Left ("Variable " ++ name ++ " not found")
-        Just (existingType, value) -> Right existingType
-
-typecheck vars (BinOp op expr1 expr2) = do
-    let exprType1 = typecheck vars expr1
-    let exprType2 = typecheck vars expr2
-    case (op, exprType1, exprType2) of
-        (Add, Right TInt, Right TInt) -> Right TInt
-        (Add, Right TString, Right TString) -> Right TString
-        (Mul, Right TInt, Right TInt) -> Right TInt
-        (Sub, Right TInt, Right TInt) -> Right TInt
-        (Div, Right TInt, Right TInt) -> Right TInt
-        (Mod, Right TInt, Right TInt) -> Right TInt
-        (Eq, Right TInt, Right TInt)  -> Right TBool
-        (Neq, Right TInt, Right TInt)  -> Right TBool
-        (Lt, Right TInt, Right TInt)  -> Right TBool
-        (Lte, Right TInt, Right TInt)  -> Right TBool
-        (Gt, Right TInt, Right TInt)  -> Right TBool
-        (Gte, Right TInt, Right TInt)  -> Right TBool
-        (Eq, Right TBool, Right TBool)  -> Right TBool
-        (Neq, Right TBool, Right TBool)  -> Right TBool
-        (Lt, Right TBool, Right TBool)  -> Right TBool
-        (Lte, Right TBool, Right TBool)  -> Right TBool
-        (Gt, Right TBool, Right TBool)  -> Right TBool
-        (Gte, Right TBool, Right TBool)  -> Right TBool
-        (Eq, Right TString, Right TString)  -> Right TBool
-        (Neq, Right TString, Right TString)  -> Right TBool
-        (And, Right TBool, Right TBool)  -> Right TBool
-        (Or, Right TBool, Right TBool)  -> Right TBool
-        (_,  Left err1, Left err2) -> Left $ err1 ++ " and " ++ err2
-        (_, Left err, _) -> Left err
-        (_, _, Left err) -> Left err
-        _                 -> Left ("Type error: " ++ show op ++ " cannot be applied to types " ++ show exprType1 ++ " and " ++ show exprType2)
-    
-typecheck vars (UnOp op expr) = do
-    let exprType = typecheck vars expr
-    case (op, exprType) of
-        (Neg, Right TInt) -> Right TInt
-        (Not, Right TBool) -> Right TBool
-        _                 -> Left ("Type error: incorrect type for " ++ show op)
-
-typecheck vars (IfElse pred expr1 expr2) = 
-    checkThrough vars TNone [(pred, TBool, "if-else statement"), (expr1, TBool, "if block"), (expr2, TBool, "else block")]
-
-typecheck vars (Seq []) = Right TNone
-typecheck vars (Seq (expr:exprs)) = do
-    checkThrough vars TNone [(expr, TNone, "sequence block")]
-    typecheck vars (Seq exprs)
-
-typecheck vars (While pred expr) = 
-    checkThrough vars TNone [(pred, TBool, "while condition"), (expr, TNone, "while loop")]
-
-typecheck vars (DoWhile pred expr) = 
-    checkThrough vars TNone [(pred, TBool, "do-while condition"), (expr, TNone, "do-while loop")]
-
-typecheck vars (Print expr) = do
-    case typecheck vars expr of 
-        Left error -> Left error
-        Right _  -> Right TNone
-
-typecheck vars x = do
-  Left $ "Syntax error: unsupported " ++ show x ++ " operation" 
-
---Helper functions for typechecking
-checkThrough :: Variables -> Type -> [(Expr, Type, String)] -> Either String Type
-checkThrough _    x []                                 = Right x
-checkThrough vars x ((expr, expectedType, failStr):es) = do
-    case typecheck vars expr of
-        Left str -> Left str
-        Right testType | testType == expectedType -> do checkThrough vars x es
-        Right x -> Left $ "Type error: incorrect type " ++ show x ++ " within " ++ failStr
